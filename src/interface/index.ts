929c143d289c0d72e58b49b8164b90115a278bbe
yuntaengtaeng <k76253218@gmail.com>
2025-03-07 14:47:31 +0900

    add : Book Type 추가

---
+++ src/interface/index.ts
@@ -0,0 +1,14 @@
+export type Book = {
+  authors: string[];
+  contents: string;
+  datetime: string;
+  isbn: string;
+  price: number;
+  publisher: string;
+  sale_price: number;
+  status: string;
+  thumbnail: string;
+  title: string;
+  translators: string[];
+  url: string;
+};
